cc4f5c6c8568a006cf874c69b314140938d78de4
Kanata Tsuda <bearpooh.kanayan@gmail.com>
2024-06-08 18:11:17 +0900

    v2_release

--- content.js
+++ content.js
@@ -1,9 +1,23 @@
-var url = window.location.href;
-var has_redirect = url.includes('sorting_setting=CHRONOLOGICAL');
-var on_groupspage = url.includes('facebook.com/groups');
-// var new_url = url + '?sorting_setting=CHRONOLOGICAL';
+function urlCheck() {
+    console.log('urlCheck');
+    var url = window.location.href;
+    var has_redirect = url.includes('CHRONOLOGICAL');
+    var is_groupspage = url.match(/.*\/groups\/[a-zA-Z0-9.]{7,}$/);
+    if (!has_redirect && is_groupspage) {
+        console.log('redirecting');
+        var new_url = url + '?sorting_setting=CHRONOLOGICAL';
+        window.location.replace(new_url);
+    }
+}
 
-if (!has_redirect && on_groupspage) {
-    var new_url = url + '?sorting_setting=CHRONOLOGICAL';
-    window.location.replace(new_url);
-} 
+document.addEventListener("DOMContentLoaded", function () {
+    urlCheck();
+});
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.message === "tab_updated") {
+            urlCheck();
+        }
+    }
+);
